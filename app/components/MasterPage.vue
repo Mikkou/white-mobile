4793d9146dc6435c7f275dc38880367bcde3c8dd
mikkou <developer.0112358@gmail.com>
2019-10-28 00:39:05 +0300

    made main page

--- app/components/MasterPage.vue
+++ app/components/MasterPage.vue
@@ -1,60 +1,83 @@
 <template>
   <Page class="page">
-    <ActionBar class="action-bar" title="Home"/>
+    <ActionBar class="action-bar" title="Welcome"/>
     <ScrollView>
       <StackLayout class="home-panel">
-        <!--Add your page content here-->
-        <Label class="h2 description-label test" text="11666555Play with NativeScript!" textWrap="true"/>
-        <Label
-          class="h2 description-label"
-          text="Write code in the editor or drag and drop components to build a NativeScript mobile application."
-          textWrap="true"
-        />
-        <Label
-          class="h2 description-label"
-          text="Scan the QR code with your mobile device and watch the changes sync live while you play with the code."
-          textWrap="true"
+        <Image
+          class="image"
+          src="~/cloud.png"
         />
 
-        <Button @tap="$navigateTo(detailPage)" class="test" text="To Details directly"/>
+        <Label
+          text="welcome in app!"
+          class="welcome-text"
+        ></Label>
+        <Button class="registration-button" @tap="$navigateTo(registryPage)" text="Регистрация"/>
+        <Button class="login-button" @tap="$navigateTo(loginPage)" text="Войти"/>
       </StackLayout>
     </ScrollView>
   </Page>
 </template>
 
 <script>
+
+  const RegistryPage = {
+    template: `
+            <Page>
+              <ActionBar title="Registration"/>
+              <GridLayout columns="auto,100" rows="auto,auto" width="300" height="300" backgroundColor="lightgray" >
+                <Label text="register form" col="0" row="0" backgroundColor="red"/>
+                <Label text="Label 2" col="1" row="0" backgroundColor="green"/>
+                <Label text="Label 3" width="150" col="0" row="1" backgroundColor="blue"/>
+              </GridLayout>
+            </Page>
+          `
+  }
+
+  const LoginPage = {
+    template: `
+            <Page>
+              <ActionBar title="Authorisation"/>
+              <StackLayout>
+                <Label text="auth form"/>
+              </StackLayout>
+            </Page>
+          `
+  }
+
   export default {
     data () {
       return {
-        detailPage: {
-          template: `
-    <Page>
-      <ActionBar title="Detail"/>
-      <StackLayout>
-        <Label text="Details.." />
-      </StackLayout>
-    </Page>
-  `
-        }
+        registryPage: RegistryPage,
+        loginPage: LoginPage
       }
-    },
+    }
   }
 </script>
 
 <style scoped>
-
-  .test {
-    color: red;
+  .image {
+    background: red;
   }
 
-
-  .home-panel {
-    vertical-align: center;
-    font-size: 20;
+  .welcome-text {
+    horizontal-align: center;
+    font-size: 30;
     margin: 15;
   }
 
-  .description-label {
-    margin-bottom: 15;
+  .registration-button {
+    background: red;
+    border-radius: 5;
+    margin: 10 30;
+    padding: 20 0;
+    color: white;
+    font-weight: 600;
   }
+
+  .login-button {
+    color: red;
+    font-weight: 600;
+  }
+
 </style>
